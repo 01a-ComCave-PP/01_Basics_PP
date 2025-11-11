a5f77aa36d29da73fae43f2b08f7be3b9d9b795f
Peter Schmidt <schmidt.multimedia@gmail.com>
2025-11-11 08:32:16 +0000

    08 / 1. Kapselung von Codeblöcken

---
+++ src/08_funktionen_I.js
@@ -0,0 +1,16 @@
+
+/***** Funktionen 01 *****/
+// 1. Kapselung von Codeblöcken
+
+
+// Funktionsaufruf | call
+// test();
+
+// Funktionsrumpf | (callee)
+// Funktionsdeklaration
+function test()
+{
+    console.log("Hallo, Kevin!");
+}
+
+
